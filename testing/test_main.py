a62af55be7db292ff6284b2cbb7f04a52ec199e7
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-18 13:17:43 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -17,24 +17,24 @@ x = np.linspace(1,6,6)
 var = randomvar( 1/6, variance=(1/6)*(5/6)/(nresamples*nsamples), vmin=0, vmax=1, isinteger=False ) 
 line1=line( x, var )
 
-axislabels=["Random variable value", "Fraction of occurances"]
+axislabels=["Outcome", "Fraction of occurances"]
 
 class UnitTests(unittest.TestCase) :
     def test_dice(self) : 
         inputs, variables = [], []
         for i in range(10) :
-            inputs.append((,))
+            inputs.append(())
             myvar = randomvar( 3.5, variance=35/12, vmin=1, vmax=6, isinteger=True )
             variables.append( myvar ) 
-        assert( check_func("dice_roll",inputs, variables ) 
+        assert( check_func("dice_roll",inputs, variables ) ) 
 
     def test_histo(self) :
         inputs, variables = [], []
         for i in range(1,5) : 
-            inputs.append(i*100)
+            inputs.append((i*100,))
             myvar = randomvar( 1/6, variance=(1/6)*(5/6)/(i*100), vmin=0, vmax=1, isinteger=False )
-            variabels.append( myvar )
-        assert( check_func("histo_estimate", inputs, variables, calls=["dice_roll"] )      
+            variables.append( myvar )
+        assert( check_func("histo_estimate", inputs, variables, calls=["dice_roll"] ) )      
 
     def test_plot(self) :
-        assert(check_plot([],exppatch=line1,explabels=axislabels,explegend=False,output=True))
+        assert(check_plot([line1],exppatch=line1,explabels=axislabels,explegend=False,output=True))
