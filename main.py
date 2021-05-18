742d41e5d6a77b8867b25574c32217bc9f33fbe5
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-18 13:17:15 +0100

    Added answer

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import numpy as np
 
 def dice_roll() :
   # Insert code so that this function return  the outcome of a roll of a fair six sided dice here.
-  
+  return np.floor( 6*np.random.uniform(0,1) ) + 1 
+ 
 def histo_estimate(n) :
   histo = np.zeros(6)
   # Insert code to compute a histogram if you roll the dice n times here.
+  for i in range(n) :
+      val = dice_roll()
+      histo[int(val-1)] = histo[int(val-1)] + 1
   
-  return histo
+  return histo / n
   
 # This tells us that 200 (nsamples) random variables should be used in the generation of each histogram
 # This procedure of generating 200 random variables and calculating the histogram should then 
@@ -28,9 +32,11 @@ for i in range(6) :
   # getting values for median - lower and median + upper so we have to 
   # do some sums to get the values of lower and upper that we want.
   lower[i] = median[i] - np.percentile( histo_samples[:,i], 5 )
-  upper[i] = 
-  
+  upper[i] = np.percentile( histo_samples[:,i], 95 ) - median[i] 
+ 
 plt.bar( [1,2,3,4,5,6], median, width=0.1 )
 # This plots the small bar around each of the values.
 plt.errorbar( [1,2,3,4,5,6], median, yerr=[lower,upper], fmt='ko' )
+plt.xlabel("Outcome")
+plt.ylabel("Fraction of occurances")
 plt.savefig("histo_with_errors.png")
